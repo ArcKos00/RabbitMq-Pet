e420bd9d5e6420e0d18654a3f699dab8f3a31eaf
Kostiantyn <kokoshugo@gmail.com>
2024-01-15 11:10:01 +0200

    Added Simple Consumer and Producer

---
+++ SimpleConsumerProducer/SimpleProducer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost", Port = 5672};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("BasicTest", false, false, false, null);
+
+const string message = "Getting started with .NET Core RabbitMQ";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "BasicTest", null, body);
+Console.WriteLine("Sent message {0}...", message);
+
+Console.WriteLine("Press [enter] to exit the Sender App");
+Console.ReadLine();
