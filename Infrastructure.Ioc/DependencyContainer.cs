04d6799c57c995aa64c68f18fcf41cf771ca8618
Kostiantyn <kokoshugo@gmail.com>
2024-01-18 23:06:33 +0200

    Added dependency cotntainer

---
+++ Infrastructure.Ioc/DependencyContainer.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Core.Bus;
+using Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Ioc
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            return services;
+        }
+    }
+}
