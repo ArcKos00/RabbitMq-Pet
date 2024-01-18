5f014185dd44faf319d122accd8d9ee8ae082a40
Kostiantyn <kokoshugo@gmail.com>
2024-01-18 22:21:16 +0200

    Implemented iterfaces and classes for Domain.core

---
+++ Domain/Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Core.Events;
+
+namespace Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
---
+++ Domain/Domain.Core/Commmands/Command.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Core.Events;
+
+namespace Domain.Core.Commmands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
